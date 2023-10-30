0f8ba47ceb0d9d6a8d18cb3115706731a2285273
Srinivasa Reddy <srinivasareddy@Srinivasas-MacBook-Pro.local>
2023-10-30 20:10:30 +0530

    New model check

---
+++ ml_model.py
@@ -0,0 +1,18 @@
+
+
+print("Adding all ML related code")
+
+def model1():
+    print("Adding code for ML Model1")
+
+def model2():
+    print("Adding code for ML Model1")
+
+def model3():
+    print("Adding code for ML Model1")
+
+
+
+#check the merge pull
+
+print("Edit")
