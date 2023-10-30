7fc65c0310d938652141e4d4c0cd0f1053628a4a
Srinivasa Reddy <srinivasareddy@Srinivasas-MacBook-Pro.local>
2023-10-30 19:53:56 +0530

    Added 3 model codes

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,12 @@
 
 
 print("Adding all ML related code")
+
+def model1():
+    print("Adding code for ML Model1")
+
+def model2():
+    print("Adding code for ML Model1")
+
+def model3():
+    print("Adding code for ML Model1")
